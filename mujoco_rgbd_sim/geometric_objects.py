d3d40b9f54843ad402383123d3a36a5d771da121
deankh <12647494+DeanKH@users.noreply.github.com>
2025-07-25 14:23:10 +0900

    refactoring

---
+++ mujoco_rgbd_sim/geometric_objects.py
@@ -0,0 +1,66 @@
+from typing import Tuple
+import xml.etree.ElementTree as ET
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+@dataclass
+class GeometricObject(ABC):
+    """Abstract base class for geometric objects in MuJoCo scene"""
+    name: str
+    position: Tuple[float, float, float] = (0, 0, 0)
+    euler: Tuple[float, float, float] = (0, 0, 0)
+    color: Tuple[float, float, float, float] = (1, 1, 1, 1)
+
+    @abstractmethod
+    def get_geom_type(self) -> str:
+        """Return the MuJoCo geometry type"""
+        pass
+
+    @abstractmethod
+    def get_size_string(self) -> str:
+        """Return the size parameter string for MuJoCo"""
+        pass
+
+    def to_xml_tree(self) -> ET.Element:
+        """Convert object to XML tree element"""
+        body_elem = ET.Element(
+            "body",
+            name=self.name,
+            pos=" ".join(map(str, self.position)),
+            euler=" ".join(map(str, self.euler)),
+        )
+        geom_elem = ET.SubElement(
+            body_elem,
+            "geom",
+            name=self.name,
+            size=self.get_size_string(),
+            type=self.get_geom_type(),
+            rgba=" ".join(map(str, self.color)),
+        )
+        return body_elem
+
+
+@dataclass
+class Box(GeometricObject):
+    """Box geometric object"""
+    size: Tuple[float, float, float] = (0.1, 0.1, 0.1)
+
+    def get_geom_type(self) -> str:
+        return "box"
+
+    def get_size_string(self) -> str:
+        return " ".join(map(str, self.size))
+
+
+@dataclass
+class Cylinder(GeometricObject):
+    """Cylinder geometric object"""
+    radius: float = 0.05
+    height: float = 0.1
+
+    def get_geom_type(self) -> str:
+        return "cylinder"
+
+    def get_size_string(self) -> str:
+        return f"{self.radius} {self.height}"
--- mujoco_rgbd_sim/mujoco_scene_builder.py
+++ mujoco_rgbd_sim/mujoco_scene_builder.py
@@ -3,63 +3,11 @@ import mujoco
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 from mujoco_rgbd_sim.mujoco_camera import Camera
-
-
-@dataclass
-class Box:
-    name: str
-    size: Tuple[float, float, float]
-    position: Tuple[float, float, float] = (0, 0, 0)
-    euler: Tuple[float, float, float] = (0, 0, 0)
-    color: Tuple[float, float, float] = (1, 1, 1, 1)
-
-    def to_xml_tree(self) -> ET.Element:
-        box_elem = ET.Element(
-            "body",
-            name=self.name,
-            pos=" ".join(map(str, self.position)),
-            euler=" ".join(map(str, self.euler)),
-        )
-        geom_elem = ET.SubElement(
-            box_elem,
-            "geom",
-            name=self.name,
-            size=" ".join(map(str, self.size)),
-            type="box",
-            rgba=" ".join(map(str, self.color)),
-        )
-        return box_elem
-
-
-@dataclass
-class Cylinder:
-    name: str
-    radius: float
-    height: float
-    position: Tuple[float, float, float] = (0, 0, 0)
-    euler: Tuple[float, float, float] = (0, 0, 0)
-    color: Tuple[float, float, float] = (1, 1, 1, 1)
-
-    def to_xml_tree(self) -> ET.Element:
-        cylinder_elem = ET.Element(
-            "body",
-            name=self.name,
-            pos=" ".join(map(str, self.position)),
-            euler=" ".join(map(str, self.euler)),
-        )
-        geom_elem = ET.SubElement(
-            cylinder_elem,
-            "geom",
-            name=self.name,
-            size=f"{self.radius} {self.height}",
-            type="cylinder",
-            rgba=" ".join(map(str, self.color)),
-        )
-        return cylinder_elem
+from mujoco_rgbd_sim.geometric_objects import GeometricObject, Box, Cylinder
 
 
 class MujocoSceneBuilder:
@@ -78,7 +26,7 @@ class MujocoSceneBuilder:
         self.xml_content = ET.fromstring(xml_template)
         return self
 
-    def add_object(self, object: Box | Cylinder):
+    def add_object(self, object: GeometricObject):
         self.objects.append(object)
         return self
 
